c7ca66d2f125b080ea38e9498c1c1e3e0c234998
Stanislav Sinyagin <ssinyagin@k-open.com>
2015-07-25 18:56:02 -0500

    distortion test script

---
+++ scripts/run_distortion_test.bash
@@ -0,0 +1,27 @@
+MYIP=`curl --silent http://ifconfig.io`
+TESTAUDIO=/etc/freeswitch/media/ITU-T_P_50_BRITISH_ENGLISH.wav
+BATCH=/tmp/distortion_test$$
+
+rm -f /var/tmp/*.wav
+
+echo >${BATCH}
+
+for y in {1..10}; do
+    echo 'bgapi originate sofia/external/rec@'${MYIP}':5080 &playback('${TESTAUDIO}')' >>${BATCH}
+done
+
+echo '/exit' >>${BATCH}
+
+for x in {1..10}; do
+    cat ${BATCH} | fs_cli -b -q >/dev/null
+    sleep 2
+done
+
+sleep 130
+
+cd /var/tmp
+
+for f in *.wav; do
+    echo -n $f
+    /etc/freeswitch/scripts/aqua_score ${TESTAUDIO} $f | grep Percent
+done
